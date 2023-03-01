cb3985884618b0ac5c8a337749b07482eb38c7e7
Dqu1J <mcpeytwot5@gmail.com>
2023-03-01 16:10:26 +0000

    update discord.js to dev branch
    hopefully will fix bots randomly going offline

--- main.js
+++ main.js
@@ -10,35 +10,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-function postFlags(flags) {
-    return {
-        method: "post",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            "token": apitoken,
-            ...flags
-        })
-    }
-}
-
-function apiFetch(url, flags, callback, callbackString) {
-    fetch(url, flags)
-    .then((res) => res.text())
-    .then((res) => {
-        if (res.startsWith("{") || res.startsWith("[")) {
-            return JSON.parse(res);
-        }
-        return res;
-    })
-    .then((res) => {
-        if (typeof res === "string") {
-            callbackString(res);
-        } else {
-            callback(res);
-        }
-    })
-}
-
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
